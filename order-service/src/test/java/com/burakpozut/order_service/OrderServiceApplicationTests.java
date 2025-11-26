80e0fb55ed9bf87d9ea1458ed0461230a4aaf64a
Burak Pozüt <burakpozut88@gmail.com>
2025-11-26 15:17:58 +0300

    Created services base

---
+++ order-service/src/test/java/com/burakpozut/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakpozut.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
